6b5f00449d4f7b804ade612dc4956d094ee7092b
Otto Kivikärki <ottoki@gmail.com>
2016-04-21 22:34:35 +0300

    Add back materialize-css

--- src/vendor.ts
+++ src/vendor.ts
@@ -8,7 +8,7 @@ import 'angular2/http';
 // RxJS
 import 'rxjs';
 
-// Other vendors for example jQuery, Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
+import './../node_modules/materialize-css/dist/js/materialize.min.js';
+import './../node_modules/materialize-css/dist/css/materialize.min.css';
 
 require.context('./public/img/mapicons', false, /\.png$/);
